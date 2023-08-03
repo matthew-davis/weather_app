4748b824d7da830b9a15cdabdceb225b72e44c32
Matthew Davis <matthewdavis@pop-os.localdomain>
2023-08-03 07:34:06 +0100

    Initial commit
    Generated by create-expo-app 2.0.3.

---
+++ App.js
@@ -0,0 +1,28 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Emily smells like <Text style={styles.fart}>farts</Text>!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 128,
+    textAlign: 'center',
+  },
+  fart: {
+    fontWeight: 'bold',
+    color: 'green',
+  },
+});
